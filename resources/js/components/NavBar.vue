4f004e312c499be58731ee17b71824cd4c461b34
Edielson Almeida <edisykes14@gmail.com>
2024-11-16 18:36:44 -0300

    ajustando o navbar

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,13 +1,13 @@
 <template>
-<header class="bg-gray-900 p-0.1 flex justify-between items-center z-40 shadow-sm px-10">
+<header class="bg-gray-900 p-0.1 flex justify-between items-center z-40 shadow-sm px-10 h-32">
+
     <div class="info-header text-white  flex w-18 p-3 gap-10 hover:text-opacity-70">
             <div class="logo">
-                <h3 class="font-bold cursor-pointer text-lg text-white"><a :href="this.route('dashboard')">
+                <h3 class="font-bold cursor-pointer text-lg text-white "><a :href="this.route('dashboard')">
                         Sistema Nexos
                     </a></h3>
             </div>
-
-        </div>
+    </div>
 
         <div class="flex items-center gap-5 ">
             <span class="material-icons hover:opacity-70 text-white cursor-pointer">
@@ -17,7 +17,7 @@
                     mail
             </span>
 
-            <logo-component size="w-10 h-10 cursor-pointer"></logo-component>
+            <logo-component size="w-20 h-20 cursor-pointer"></logo-component>
 
         </div>
     </header>
